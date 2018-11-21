6237e25b6fb8b0a237afa2fa9d710d43c2aa2bc2
Soetnowzen <BaraFinkel@gmail.com>
2018-11-21 08:30:35 +0100

    Added argument level for ssh completion.

--- bash/_known_hosts.bash
+++ bash/_known_hosts.bash
@@ -1,13 +1,15 @@
 function _known_hosts()
 {
-  hosts=$(grep '\<Host\>' ~/.ssh/config)
-  results=""
-  for host in $hosts; do
-    if [[ "$host" != "Host" ]]; then
-      results+="$host "
-    fi
-  done
-  COMPREPLY=($(compgen -W "$results" "${COMP_WORDS[1]}"))
+  if [[ $COMP_CWORD -eq 1 ]]; then
+    hosts=$(grep '\<Host\>' ~/.ssh/config)
+    results=""
+    for host in $hosts; do
+      if [[ "$host" != "Host" ]]; then
+        results+="$host "
+      fi
+    done
+    COMPREPLY=($(compgen -W "$results" "${COMP_WORDS[1]}"))
+  fi
 }
 
 complete -F _known_hosts ssh
