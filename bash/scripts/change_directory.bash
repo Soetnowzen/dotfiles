a68f5a15576b678701fe1089c0fe30fe578d3be4
Soetnowzen <BaraFinkel@gmail.com>
2018-11-23 20:46:33 +0100

    Change the argument from numbers to directory names.

--- bash/scripts/change_directory.bash
+++ bash/scripts/change_directory.bash
@@ -3,21 +3,28 @@ unalias ... 2> /dev/null
 
 function ..()
 {
-  local limit=$1
-  if [[ $limit == "" ]]; then
-    limit=1
+  local destination=$1
+  if [[ $destination == "" ]]; then
+    cd .. || exit
+  else
+    local new_pwd=$PWD
+    local directories=$(pwd | tr '/' '\n' | tac | tr '\n' ' ')
+    for directory in $directories; do
+      if [[ $destination != $directory ]]; then
+        new_pwd+="/.."
+      else
+        break
+      fi
+    done
+    cd "$new_pwd" || exit
   fi
-  local new_pwd=$PWD
-  for ((i=1; i<= limit; i++)); do
-    new_pwd+="/.."
-  done
-  cd "$new_pwd" || exit
 }
 
 function _change_directory_completion()
 {
   if [[ $COMP_CWORD == 1 ]]; then
-    COMPREPLY=($(compgen -W "1 2 3 4 5" "${COMP_WORDS[1]}"))
+    directories=$(pwd | tr '/' ' ')
+    COMPREPLY=($(compgen -W "$directories" "${COMP_WORDS[1]}"))
   fi
 }
 
