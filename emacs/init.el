26c939b43f15792f03d288d46e6efeb35eae45ee
Soetnowzen <BaraFinkel@gmail.com>
2019-01-23 14:44:20 +0100

    Added the solarized repo, but want to fetch automaticlly.

--- emacs/init.el
+++ emacs/init.el
@@ -1,6 +1,7 @@
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
+;; (add-to-list 'package-archives '("github" . "https://github.com/sellout/emacs-color-theme-solarized"))
 (package-initialize)
 
 (add-to-list 'load-path "~/.emacs.d/scripts")
