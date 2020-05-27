c462c21803168a845df963490ba1f3d564dbeb8d
Sebastian Lagerman <eseblag@seroiuts01170.sero.gic.ericsson.se>
2020-05-27 11:47:05 +0200

    Added -X & -Y to ssh completion.

--- bash/scripts/ssh_completion.bash
+++ bash/scripts/ssh_completion.bash
@@ -1,15 +1,41 @@
+function _get_hosts()
+{
+  local results=""
+  local hosts=$(grep '\<Host\>' ~/.ssh/config)
+  for host in $hosts; do
+    if [[ "$host" != "Host" ]]; then
+      results+="$host "
+    fi
+  done
+  echo "$results"
+}
+
 function _ssh_completion()
 {
-  if [[ $COMP_CWORD -eq 1 ]]; then
-    hosts=$(grep '\<Host\>' ~/.ssh/config)
-    results=""
-    for host in $hosts; do
-      if [[ "$host" != "Host" ]]; then
-        results+="$host "
-      fi
-    done
-    COMPREPLY=($(compgen -W "$results" "${COMP_WORDS[1]}"))
+  local current previous
+  current=${COMP_WORDS[COMP_CWORD]}
+  previous=${COMP_WORDS[COMP_CWORD-1]}
+
+  if [[ $COMP_CWORD -gt 2 ]]; then
+    return
+  fi
+  if [[ $current == -* ]]; then
+    local options="-Y -X"
+    COMPREPLY=($(compgen -W "$options" -- "$current"))
+    [[ $COMPREPLY == *= ]] && compopt -o nospace
+    return
+  else
+    # local results="-Y -X "
+    local results=""
+    if [[ $previous == -* ]]; then
+      results=$(_get_hosts)
+    elif [[ $previous == "ssh" ]]; then
+      results="-Y -X $(_get_hosts)"
+    else
+      results="-Y -X"
+    fi
+    COMPREPLY=($(compgen -W "$results" -- "$current"))
   fi
 }
 
-complete -F _ssh_completion ssh
+complete -o default -F _ssh_completion ssh
