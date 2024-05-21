9ccb08708f77ca621d0da6240142a3eff7dfa67e
Sebastian Lagerman <Sebastian.Lagerman@fortnox.se>
2024-05-21 15:53:48 +0200

    Installed bc before linking bash to avoid error message.

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
+sudo apt-get update && sudo apt-get install -y bc
 ln -fsn ~/dotfiles/bash/my.bashrc ~/.bashrc
 ln -fsn ~/dotfiles/input/.inputrc ~/.inputrc
-sudo apt-get update && sudo apt-get install -y bc
